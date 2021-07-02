3f2b29b582ec7a5b65a950e1635d3695bc275084
Wellington Oliveira dos Santos <welblade@gmail.com>
2021-07-02 03:14:31 -0300

    Teste de funcionalidades com listas

---
+++ src/main/kotlin/com/github/welblade/collections/TesteList.kt
@@ -0,0 +1,25 @@
+package com.github.welblade.collections
+
+data class Worker (
+    val name:String,
+    val wage:Double
+){
+    override fun toString(): String {
+        return """
+            Nome:    $name
+            Salário: $wage
+        """.trimIndent()
+    }
+}
+
+fun main() {
+    val joao = Worker("João", 2000.0)
+    val pedro = Worker("Pedro", 1000.0)
+    val maria = Worker("Maria", 4000.0)
+
+    val workers = listOf(joao, pedro, maria)
+    workers.forEach { println(it) }
+    println("------------------------------------")
+    val findMary = workers.find { it.name == "Maria" }
+    println( findMary )
+}
