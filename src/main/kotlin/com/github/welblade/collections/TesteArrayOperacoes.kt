1d996ca2970b460c5dd7bcd6cb1857839e9fdfa1
Wellington Oliveira dos Santos <welblade@gmail.com>
2021-07-02 02:42:57 -0300

    Teste de funcionalidades com DoubleArray, algumas operações como count, find e any

--- src/main/kotlin/com/github/welblade/collections/TesteArrayOperacoes.kt
+++ src/main/kotlin/com/github/welblade/collections/TesteArrayOperacoes.kt
@@ -13,4 +13,13 @@ fun main() {
     println("----------------------------------------")
     val wagesGreaterThan2k = wages.filter { it > 2000.0 }
     wagesGreaterThan2k.forEachIndexed {  index, d -> print("[$index] = $d, ") }
+
+    println("\n----------------------------------------")
+    println("${wages.count { it in 2000.0..5000.0 }}")
+    println("----------------------------------------")
+    println("${wages.find { it == 2250.0 }}")
+    println("${wages.find { it == 250.0 }}")
+    println("----------------------------------------")
+    println("${wages.any { it == 2250.0 }}")
+    println("${wages.any { it == 250.0 }}")
 }
